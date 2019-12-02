161ef13afdbd5fafde4034c7496c8a6592198575
Abdul Rehman <Abdul.Rehman@ey.com>
2019-12-02 17:09:00 -0500

    Add project files.

---
+++ ConsulFun/Startup.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Consul;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Options;
+
+namespace ConsulFun
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.Configure<ConsulConfig>(Configuration.GetSection("consulConfig"));
+            services.AddSingleton<IConsulClient, ConsulClient>(p => new ConsulClient(consulConfig =>
+            {
+                var address = Configuration["consulConfig:address"];
+                consulConfig.Address = new Uri(address);
+            }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory, IHostApplicationLifetime lifetime)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            //RegisterWithConsul(app, lifetime);
+        }
+
+        public static IApplicationBuilder RegisterWithConsul(IApplicationBuilder app, IHostApplicationLifetime lifetime)
+        {
+            // Retrieve Consul client from DI
+            var consulClient = app.ApplicationServices
+                                .GetRequiredService<IConsulClient>();
+            var consulConfig = app.ApplicationServices
+                                .GetRequiredService<IOptions<ConsulConfig>>();
+            // Setup logger
+            var loggingFactory = app.ApplicationServices
+                                .GetRequiredService<ILoggerFactory>();
+            var logger = loggingFactory.CreateLogger<IApplicationBuilder>();
+
+            // Get server IP address
+            var features = app.Properties["server.Features"] as FeatureCollection;
+            var addresses = features.Get<IServerAddressesFeature>();
+            var address = addresses.Addresses.First();
+
+            // Register service with consul
+            var uri = new Uri(address);
+            var registration = new AgentServiceRegistration()
+            {
+                ID = $"{consulConfig.Value.ServiceId}-{uri.Port}",
+                Name = consulConfig.Value.ServiceName,
+                Address = $"{uri.Scheme}://{uri.Host}",
+                Port = uri.Port,
+                Tags = new[] { "Students", "Courses", "School" }
+            };
+
+            logger.LogInformation("Registering with Consul");
+            consulClient.Agent.ServiceDeregister(registration.ID).Wait();
+            consulClient.Agent.ServiceRegister(registration).Wait();
+
+            lifetime.ApplicationStopping.Register(() =>
+            {
+                logger.LogInformation("Deregistering from Consul");
+                consulClient.Agent.ServiceDeregister(registration.ID).Wait();
+            });
+
+            return app;
+        }
+    }
+
+    public class ConsulConfig
+    {
+        public string ServiceAddress { get; set; }
+        public string ServiceId { get; set; }
+
+        public string ServiceName { get; set; }
+
+        public string[] Tags { get; set; }
+    }
+}
